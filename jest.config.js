e23a458133342d6e5ac9664e8d4a2d2deaa456fc
Anton Sylantiev <anton.sylantiev@intellias.com>
2026-05-05 17:10:15 +0300

    Add github action and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,24 @@
 
+const dbType = process.env.DB_TYPE || 'unknown';
+
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./test/setup.js'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: `./reports/${dbType}`,
+        filename: 'report.html',
+        expand: true
+      }
+    ],
+    [
+      'jest-junit',
+      {
+        outputDirectory: `./reports/${dbType}`,
+        outputName: 'junit.xml'
+      }
+    ]
+  ]
 };
